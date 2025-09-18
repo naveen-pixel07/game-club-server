627e327d8c2f05e578009ecd74b22a774d6b5f6b
Naveen <naveenknani2580@gmail.com>
2025-09-18 11:01:13 +0530

    game-club

--- gameclub/src/main/java/com/gameclub/gameclub/repository/MemberRepository.java
+++ gameclub/src/main/java/com/gameclub/gameclub/repository/MemberRepository.java
@@ -1,5 +1,10 @@
 package com.gameclub.gameclub.repository;
 
-public class MemberRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.gameclub.gameclub.model.Member;
+
+@Repository
+public interface MemberRepository extends MongoRepository<Member, String> {
     
 }
